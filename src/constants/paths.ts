e922f3d60a5a6fb30da9037fb376a32ee50b4bd8
mizukenvivid <kenji.mizuguchi@vivid-studio.jp>
2023-12-05 20:31:36 +0900

    feat: 定番HooksのPathを設定

---
+++ src/constants/paths.ts
@@ -0,0 +1,23 @@
+export const paths = [
+  {
+    label: "useLocalStorage: ブラウザのローカルストレージにデータを保存し、Reactのstateと同期する",
+    path: "/useLocalStorage",
+  },
+  {
+    label:
+      "useDebounce: 入力などのアクションに対してディレイを設定し、高頻度で発生するイベントの処理を制御する",
+    path: "/useDebounce",
+  },
+  {
+    label: "useInterval: 指定された間隔で関数を繰り返し実行する",
+    path: "/useInterval",
+  },
+  {
+    label: "useClipboard: ユーザーのクリップボードにテキストをコピーする",
+    path: "/useClipboard",
+  },
+  {
+    label: "useAuth: 認証状態を管理する",
+    path: "/useAuth",
+  },
+];
